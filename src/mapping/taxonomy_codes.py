5e6dd07e61095b4896a21b6d9c8651b129d39673
Mohammad Khalaf <mkhalaf1999@gmail.com>
2025-11-17 20:31:01 -0600

    Fix taxonomy codes for NPPES API compatibility

--- src/mapping/taxonomy_codes.py
+++ src/mapping/taxonomy_codes.py
@@ -1,6 +1,10 @@
-"""Taxonomy code definitions for healthcare specialties."""
+"""Taxonomy code definitions for healthcare specialties.
 
-# Common taxonomy codes mapped to specialty names
+These descriptions match what the NPPES API accepts for taxonomy_description parameter.
+Based on testing, the API accepts shorter, simpler descriptions.
+"""
+
+# Common taxonomy codes mapped to NPPES API-accepted descriptions
 TAXONOMY_CODES = {
     "207Q00000X": "Family Medicine",
     "207R00000X": "Internal Medicine",
@@ -12,7 +16,7 @@ TAXONOMY_CODES = {
     "204F00000X": "Neurology",
     "207T00000X": "Emergency Medicine",
     "207W00000X": "Ophthalmology",
-    "207X00000X": "Orthopedic Surgery",
+    "207X00000X": "Orthopedic",  # API accepts "Orthopedic" not "Orthopedic Surgery"
     "207Y00000X": "Otolaryngology",
     "208100000X": "Physical Medicine & Rehabilitation",
     "208200000X": "Anesthesiology",
@@ -22,9 +26,10 @@ TAXONOMY_CODES = {
     "208600000X": "Geriatric Psychiatry",
     "208800000X": "Urology",
     "208C00000X": "Colon & Rectal Surgery",
-    "208D00000X": "General Practice",
-    "208600000X": "Geriatric Psychiatry",
+    "207RG0100X": "Gastroenterology",
+    "207RC0000X": "Cardiology",
+    "207N00000X": "Dermatology",
+    "2085R0202X": "Radiology",
     "363A00000X": "Physician Assistant",
     "363L00000X": "Nurse Practitioner",
 }
-
